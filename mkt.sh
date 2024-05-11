9e4ad634c617675f8405b957b0a26bd26ef3d3f0
JennValentine <163698020+JennValentine@users.noreply.github.com>
2024-05-10 22:20:59 -0400

    mkt.sh

--- mkt.sh
+++ mkt.sh
@@ -9,6 +9,12 @@
 
 # Paleta de colores
 reset="\033[0m"       # Restablecer todos los estilos y colores
+bold="\033[1m"        # Texto en negrita
+italic="\033[3m"      # Texto en cursiva
+underline="\033[4m"   # Texto subrayado
+blink="\033[5m"       # Texto parpadeante
+reverse="\033[7m"     # Invertir colores de fondo y texto
+hidden="\033[8m"      # Texto oculto (generalmente invisible)
 
 # Colores de texto
 black="\033[0;30m"     # Negro
@@ -20,6 +26,16 @@ magenta="\033[0;35m"   # Magenta
 cyan="\033[0;36m"      # Cian
 white="\033[0;37m"     # Blanco
 
+# Colores de fondo
+bg_black="\033[0;40m"     # Fondo Negro
+bg_red="\033[0;41m"       # Fondo Rojo
+bg_green="\033[0;42m"     # Fondo Verde
+bg_yellow="\033[0;43m"    # Fondo Amarillo
+bg_blue="\033[0;44m"      # Fondo Azul
+bg_magenta="\033[0;45m"   # Fondo Magenta
+bg_cyan="\033[0;46m"      # Fondo Cian
+bg_white="\033[0;47m"     # Fondo Blanco
+
 # Iconos
 checkmark="${green}[++]${reset}"
 error="${red}[--]${reset}"
@@ -29,6 +45,7 @@ indicator="${red}==>${reset}"
 
 # Barra de separación
 barra="${blue}|--------------------------------------------|${reset}"
+bar="${yellow}--------------------------------------------${reset}"
 
 # Definición de la función "mkt"
 function mkt ()
@@ -51,6 +68,7 @@ function mkt ()
     echo -e "\n${checkmark} ${green}Folders Created Successfully"
 
     echo -e "\n${yellow}${info} ${white}GITHUB OFICIAL: ${green}https://github.com/JennValentine/Directorio-mkt\n"
+    echo -e "$barra"
 }
 
 # Llamada a la función "mkt"
